03097909db903b5f2f43acb14ef31c0a8fdbcf35
Katherine Stevens Andersen <KAstev@gmail.com>
2023-02-08 20:30:37 -0700

    Clean up formatting

--- tools/neb_raw_data_fetch.py
+++ tools/neb_raw_data_fetch.py
@@ -6,35 +6,48 @@ from kauffman.data_fetch import bfs, pep, bds
 
 
 def raw_data_update():
-    joblib.dump(str(pd.to_datetime('today')), c.filenamer('data/raw_data/raw_data_fetch_time.pkl'))
+    joblib.dump(
+        str(pd.to_datetime('today')), 
+        c.filenamer('data/raw_data/raw_data_fetch_time.pkl')
+    )
 
     for region in ['us', 'state']:
-        bfs(['BA_BA', 'BF_SBF8Q', 'BF_DUR8Q'], region, annualize=True). \
-            rename(columns={'BF_DUR8Q': 'avg_speed_annual', 'BF_SBF8Q': 'bf', 'BA_BA': 'ba'}).\
-            to_csv(c.filenamer(f'data/raw_data/bfs_{region}.csv'), index=False)
+        bfs(['BA_BA', 'BF_SBF8Q', 'BF_DUR8Q'], region, annualize=True) \
+            .rename(columns={
+                'BF_DUR8Q': 'avg_speed_annual', 'BF_SBF8Q': 'bf', 'BA_BA': 'ba'
+            }) \
+            .to_csv(c.filenamer(f'data/raw_data/bfs_{region}.csv'), index=False)
 
-        bfs(['BF_SBF8Q'], region, march_shift=True). \
-            rename(columns={'BF_SBF8Q': 'bf_march_shift'}). \
-            to_csv(c.filenamer(f'data/raw_data/bfs_march_{region}.csv'), index=False)
+        bfs(['BF_SBF8Q'], region, march_shift=True) \
+            .rename(columns={'BF_SBF8Q': 'bf_march_shift'}) \
+            .to_csv(
+                c.filenamer(f'data/raw_data/bfs_march_{region}.csv'), 
+                index=False
+            )
 
-        pep(region). \
-            rename(columns={'POP': 'population'}). \
-            astype({'time': 'int', 'population': 'int'}).\
-            to_csv(c.filenamer(f'data/raw_data/pep_{region}.csv'), index=False)
+        pep(region) \
+            .rename(columns={'POP': 'population'}) \
+            .astype({'time': 'int', 'population': 'int'}) \
+            .to_csv(c.filenamer(f'data/raw_data/pep_{region}.csv'), index=False)
 
-        bds(['FIRM'], geo_level=region). \
-            rename(columns={'FIRM': 'firms'}).\
-            to_csv(c.filenamer(f'data/raw_data/bds_{region}.csv'), index=False)
+        bds(['FIRM'], geo_level=region) \
+            .rename(columns={'FIRM': 'firms'}) \
+            .to_csv(c.filenamer(f'data/raw_data/bds_{region}.csv'), index=False)
 
 
 def s3_update():
     files_lst = [
-        'raw_data_fetch_time.pkl', 'bfs_us.csv', 'bfs_march_us.csv', 'pep_us.csv', 'bds_us.csv', 'bfs_state.csv',
-        'bfs_march_state.csv', 'pep_state.csv', 'bds_state.csv'
+        'raw_data_fetch_time.pkl', 'bfs_us.csv', 'bfs_march_us.csv', 
+        'pep_us.csv', 'bds_us.csv', 'bfs_state.csv', 'bfs_march_state.csv', 
+        'pep_state.csv', 'bds_state.csv'
     ]
 
     for file in files_lst:
-        file_to_s3(c.filenamer(f'data/raw_data/{file}'), 'emkf.data.research', f'indicators/neb/raw_data/{file}')
+        file_to_s3(
+            c.filenamer(f'data/raw_data/{file}'), 
+            'emkf.data.research', 
+            f'indicators/neb/raw_data/{file}'
+        )
 
 
 def main():
